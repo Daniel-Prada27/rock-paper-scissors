719cd99b3d01a719a8b053ea8b5afe38bb38a8a3
Daniel <danielspradah0@gmail.com>
2022-10-01 15:53:57 -0500

    Fix bug Fixed a bug where computer would choice the same option over and over

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 const options = ["Rock", "Paper", "Scissors"];
-let optionPicker = Math.floor(Math.random()*options.length);
-let computerChoice = options[optionPicker];
+// let computerChoice;
 let userChoice;
+
+let userWinCount = 0;
+let computerWinCount = 0;
 
 function getUserChoice() {
     userChoice = window.prompt("Enter your choice: ");
@@ -14,9 +16,15 @@ function getUserChoice() {
     return userChoice;
 }
 
+function getComputerChoice(){
+    let optionPicker = Math.floor(Math.random()*options.length);
+    let computerChoice = options[optionPicker];
+    return computerChoice;
+}
+
 function round(){
     let userChoice = getUserChoice();
-
+    let computerChoice = getComputerChoice();
     console.log("User choice: " + userChoice);
     console.log("Computer choice: " + computerChoice);
 
@@ -25,20 +33,26 @@ function round(){
     } else if (userChoice == "Rock") {
         if (computerChoice == "Paper") {
             console.log("Computer wins");
+            computerWinCount++;
         } else if (computerChoice == "Scissors"){
             console.log("User wins");
+            userWinCount++;
         }
     } else if (userChoice == "Scissors"){
         if (computerChoice == "Rock"){
             console.log("Computer wins");
+            computerWinCount++;
         } else if (computerChoice == "Paper"){
             console.log("User wins");
+            userWinCount++;
         }
     } else if (userChoice == "Paper"){
         if (computerChoice == "Rock"){
             console.log("User wins");
+            userWinCount++;
         } else if (computerChoice == "Scissors"){
             console.log("Computer wins");
+            computerWinCount++;
         }
     }
 }
@@ -47,6 +61,20 @@ function game(numberOfRounds){
     for (i=1; i <= numberOfRounds; i++ ){
         round();
     }
+
+    console.log("User wins: " + userWinCount);
+    console.log("Computer wins: " + computerWinCount);
+    console.log(WhoWon() + " is the winner!!!");
+}
+
+function WhoWon(){
+    let finalWinner;
+    if (userWinCount > computerWinCount){
+        finalWinner = "User";
+    } else {
+        finalWinner = "Computer";
+    }
+    return finalWinner;
 }
 
 
