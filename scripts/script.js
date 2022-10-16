8d52ec39c6ed2449f7b14f79b49d4ee336dd7adc
Daniel <danielspradah0@gmail.com>
2022-10-16 13:08:09 -0500

    Make buttons work

--- scripts/script.js
+++ scripts/script.js
@@ -2,72 +2,84 @@ const rockBtn = document.getElementById('rock-btn');
 const paperBtn = document.getElementById('paper-btn');
 const scissorsBtn = document.getElementById('scissors-btn');
 
+const options = ["Rock", "Paper", "Scissors"];
+let computerChoice;
+let userChoice;
+
+let userWinCount = 0;
+let computerWinCount = 0;
+let roundCount = 1;
 
 
+rockBtn.addEventListener('click', () => {
+    userChoice = "Rock";
+    round();
+});
 
+paperBtn.addEventListener('click', () => {
+    userChoice = "Paper";
+    round();
+});
 
-// const options = ["Rock", "Paper", "Scissors"];
-// // let computerChoice;
-// let userChoice;
+scissorsBtn.addEventListener('click', () => {
+    userChoice = "Scissors";
+    round();
+});
 
-// let userWinCount = 0;
-// let computerWinCount = 0;
-// let roundCount = 1;
+function getUserChoice() {
+    if(document.getElementById('rock-btn').clicked == true){
+        userChoice = "Rock";
+    }else if(document.getElementById('paper-btn').clicked == true){
+        userChoice = "Paper";
+    }else if(document.getElementById('scissors-btn').clicked == true) {
+        userChoice = "Scissors";
+    }
+    return userChoice;
+}
 
-// function getUserChoice() {
-//     userChoice = window.prompt("Enter your choice: ");
-//     userChoice = userChoice.charAt(0).toUpperCase() + userChoice.slice(1).toLowerCase();
-//     if (userChoice != "Rock" && userChoice != "Paper" && userChoice != "Scissors"){
-//         alert("Try again");
-//         getUserChoice();
-//     }
+function getComputerChoice(){
+    let optionPicker = Math.floor(Math.random()*options.length);
+    let computerChoice = options[optionPicker];
+    return computerChoice;
+}
 
-//     return userChoice;
-// }
+function round(){
+    let userChoice = getUserChoice();
+    let computerChoice = getComputerChoice();
 
-// function getComputerChoice(){
-//     let optionPicker = Math.floor(Math.random()*options.length);
-//     let computerChoice = options[optionPicker];
-//     return computerChoice;
-// }
+    console.log("Round number: " + roundCount);
+    roundCount++;
+    console.log("User choice: " + userChoice);
+    console.log("Computer choice: " + computerChoice);
 
-// function round(){
-//     let userChoice = getUserChoice();
-//     let computerChoice = getComputerChoice();
-
-//     console.log("Round number: " + roundCount);
-//     roundCount++;
-//     console.log("User choice: " + userChoice);
-//     console.log("Computer choice: " + computerChoice);
-
-//     if (userChoice == computerChoice){
-//         console.log("Draw");
-//     } else if (userChoice == "Rock") {
-//         if (computerChoice == "Paper") {
-//             console.log("Computer wins");
-//             computerWinCount++;
-//         } else if (computerChoice == "Scissors"){
-//             console.log("User wins");
-//             userWinCount++;
-//         }
-//     } else if (userChoice == "Scissors"){
-//         if (computerChoice == "Rock"){
-//             console.log("Computer wins");
-//             computerWinCount++;
-//         } else if (computerChoice == "Paper"){
-//             console.log("User wins");
-//             userWinCount++;
-//         }
-//     } else if (userChoice == "Paper"){
-//         if (computerChoice == "Rock"){
-//             console.log("User wins");
-//             userWinCount++;
-//         } else if (computerChoice == "Scissors"){
-//             console.log("Computer wins");
-//             computerWinCount++;
-//         }
-//     }
-// }
+    if (userChoice == computerChoice){
+        console.log("Draw");
+    } else if (userChoice == "Rock") {
+        if (computerChoice == "Paper") {
+            console.log("Computer wins");
+            computerWinCount++;
+        } else if (computerChoice == "Scissors"){
+            console.log("User wins");
+            userWinCount++;
+        }
+    } else if (userChoice == "Scissors"){
+        if (computerChoice == "Rock"){
+            console.log("Computer wins");
+            computerWinCount++;
+        } else if (computerChoice == "Paper"){
+            console.log("User wins");
+            userWinCount++;
+        }
+    } else if (userChoice == "Paper"){
+        if (computerChoice == "Rock"){
+            console.log("User wins");
+            userWinCount++;
+        } else if (computerChoice == "Scissors"){
+            console.log("Computer wins");
+            computerWinCount++;
+        }
+    }
+}
 
 // function game(numberOfRounds){
 //     for (i=1; i <= numberOfRounds; i++ ){
